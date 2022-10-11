4da26072722db0789ac992e15858ca1983835630
tranhoangdang <tranhoangdang1402@gmail.com>
2022-10-12 00:28:58 +0700

    make it better

--- database/migrations/2022_10_10_073457_create_groups_table.php
+++ database/migrations/2022_10_10_073457_create_groups_table.php
@@ -14,7 +14,7 @@ class CreateGroupsTable extends Migration
     public function up()
     {
         Schema::create('groups', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
             $table->string('name');
             $table->boolean('is_active')->default(true);
             $table->boolean('is_delete')->default(false);
